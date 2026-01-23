e7c1318048210de2ef7cbc39d43637a70aba6f4f
Carlos Alexandre Pires de Carvalho Junior <capcj@protonmail.com>
2026-01-23 17:29:39 -0300

    feat(batch-9): main exports

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { Analyzer } from './core/analyzer.js';
+export { Assessor } from './core/assessor.js';
+export { Scaffolder } from './core/scaffolder.js';
+export { Reporter } from './core/reporter.js';
+export * from './types.js';
+export * from './constants.js';
